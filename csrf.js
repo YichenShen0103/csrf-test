93e93bfc0a1381764f0f06660da25664d281ae4d
ycshenMac <shenchiri576@gmail.com>
2025-05-08 17:23:33 +0800

    3.1

--- csrf.js
+++ csrf.js
@@ -1,5 +1,6 @@
-const firstScript = document.querySelector("body script");
-const scriptContent = firstScript.innerHTML;
-const match = scriptContent.match(/csrf_token=([a-zA-Z0-9]+)/);
-const csrfToken = match[1];
-alert(csrfToken);
+// const firstScript = document.querySelector("body script");
+// const scriptContent = firstScript.innerHTML;
+// const match = scriptContent.match(/csrf_token=([a-zA-Z0-9]+)/);
+// const csrfToken = match[1];
+let token = document.querySelector('input[name="csrf_token"]').value;
+alert(token);
